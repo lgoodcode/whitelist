23b4bd870a93d4cb02ea368f1882f5b6947df14a
Lawrence Good <lawrence.good55@gmail.com>
2022-02-14 13:45:45 -0800

    v0.28.1 Update tests, auto-merge workflow
    Had to include the Material UI ThemeProvider component.
    
    Fixed error for the secret where it has to be wrapped in quotes.

--- src/__tests__/pages/Home.test.tsx
+++ src/__tests__/pages/Home.test.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material/styles'
 import { render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import Home from 'pages/Home'
 import store from 'app/store'
+import { StyleTheme } from 'assets/theme'
 
 // Need to import it manually because it has to set the window
 // property, whereas jest.mock() simply imports the module
@@ -14,7 +16,9 @@ test('renders home page without crashing', () => {
    render(
       <Provider store={store}>
          <BrowserRouter>
-            <Home />
+            <ThemeProvider theme={StyleTheme('dark')}>
+               <Home />
+            </ThemeProvider>
          </BrowserRouter>
       </Provider>
    )
